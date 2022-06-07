757219b5c2e596fd575f32685e937b1519d5197b
Varun Tanna <varun_tanna@ymail.com>
2022-06-07 14:34:10 -0400

    drinks.js and refactored html

--- assets/drinksScript.js
+++ assets/drinksScript.js
@@ -83,27 +83,36 @@ var init = function () {
                 return response.json();
             })
             .then(function (data) {
-                // console.log(data.drinks[0])
-                var ingredients = getIngredients(data.drinks[0]);
-                console.log(ingredients)
-                // var articleEl = document.createElement("article");
-                // articleEl.className = "card p-3 my-3 bg-dark text-light";
-                var pEl = document.createElement('p')
-                // pEl.className = "card p-3 bg-dark text-light";
-                pEl.textContent = data.drinks[0].strDrink
-                var instructions = document.createElement('p')
-                instructions.textContent = data.drinks[0].strInstructions
-                // articleEl.appendChild(pEl);
-                // articleEl.appendChild(instructions);
-                // articleEl.appendChild(ingredients);
-                searchResultsEl.appendChild(pEl)
-                searchResultsEl.appendChild(instructions)
+                console.log(data) 
+                var drink = data.drinks[0];
+                var ingredients = getIngredients(drink);
+                var ulEl = document.createElement('ul');
                 for (var ingredient of ingredients) {
-                    var ingredientsEl = document.createElement('p')
-                    ingredientsEl.textContent = ingredient
-                    searchResultsEl.appendChild(ingredientsEl)
-    
+                    var liEl = document.createElement('li');
+                    liEl.textContent = ingredient;
+                    ulEl.appendChild(liEl);
                 }
+                console.log(drink)
+
+                var articleEl = document.createElement('article');
+                articleEl.className = "card p-3 my-3 bg-dark text-light";
+
+                var h3El = document.createElement('h3');
+                h3El.textContent = drink.strDrink;
+
+                var pEl = document.createElement('p');
+                pEl.textContent = drink.strInstructions;
+
+                var btnEl = document.createElement('a');
+                btnEl.className = "btn btn-light text-dark";
+                btnEl.textContent = "Click Image";
+                btnEl.setAttribute('href', drink.strDrinkThumb);
+
+                articleEl.appendChild(h3El);
+                articleEl.appendChild(pEl);
+                articleEl.appendChild(ulEl);
+                searchResultsEl.appendChild(articleEl);
+                articleEl.appendChild(btnEl);
             }) 
             .catch(function (error) {
                 console.log(error);
