b090965c5b6d19c98369f7a4bd9b792bdb411738
Varun Tanna <varun_tanna@ymail.com>
2022-06-08 15:14:35 -0400

    youtube flaticon

--- assets/foodScript.js
+++ assets/foodScript.js
@@ -95,7 +95,7 @@ function getApi(event) {
                 var pEl = document.createElement('p');
                 pEl.textContent = meal.strInstructions;
 
-                var btnEl = document.createElement('a');
+                var btnEl = document.createElement('img');
                 btnEl.className = "btn btn-light text-dark";
                 // btnEl.textContent = "Click for Youtube video!";
                 btnEl.setAttribute('src', URL('youtube.png'))
@@ -139,17 +139,20 @@ var init = function () {
                 var pEl = document.createElement('p');
                 pEl.textContent = meal.strInstructions;
 
-                var btnEl = document.createElement('a');
-                btnEl.className = "btn btn-light text-dark";
-                btnEl.textContent = "Click for Youtube video!";
-                // btnEl.setAttribute(src = 'youtube.png')
-                btnEl.setAttribute('href', meal.strYoutube);
-
+                var btnEl = document.createElement('img');
+                btnEl.className = "youtubeimg";
+                btnEl.setAttribute('src', './youtube.png');
+                
+                var aEl = document.createElement('a');
+                aEl.setAttribute('href', meal.strYoutube);
+                aEl.classList.add("d-flex", "justify-content-center");
+                aEl.appendChild(btnEl);
+                
                 articleEl.appendChild(h3El);
                 articleEl.appendChild(pEl);
                 articleEl.appendChild(ulEl);
                 searchResultsEl.appendChild(articleEl);
-                articleEl.appendChild(btnEl);
+                articleEl.appendChild(aEl);
              })
              .catch(function (error) {
                  console.log(error);
